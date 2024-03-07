c69ccbdb79f4f5533c19c281b2508644ad65876f
Carlos - San Pablo <intranet@istsanpablo.edu.pe>
2024-03-07 12:19:01 -0500

    adding carrousel to the final project

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Js responsive Menu
+const navLinks = document.querySelector('.nav-links')
+
+function onToggleMenu(e) {
+    e.name = e.name === 'menu' ? 'close' : 'menu'
+    navLinks.classList.toggle('top-[9%]')
+}
